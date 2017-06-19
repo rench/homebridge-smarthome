8f6aa8fadd75e5ded55d7621c1a55e9e8a020796
Wang.ch <Wang.ch@gaiasys.cn>
2017-06-19 19:42:09 +0800

    add mijia monitor support

--- kit/mijia/monitor.js
+++ kit/mijia/monitor.js
@@ -1,9 +1,63 @@
 const Base = require('./base');
+let PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
 class Monitor extends Base {
   constructor(mijia) {
     super(mijia);
+    PlatformAccessory = mijia.PlatformAccessory;
+    Accessory = mijia.Accessory;
+    Service = mijia.Service;
+    Characteristic = mijia.Characteristic;
+    UUIDGen = mijia.UUIDGen;
   }
-  parseMsg(msg, rinfo) {
-    
+  /**
+ * parse the gateway json msg
+ * @param {*json} json 
+ * @param {*remoteinfo} rinfo 
+ */
+  parseMsg(json, rinfo) {
+    let { cmd, model, sid } = json;
+    let data = JSON.parse(json.data);
+    let { voltage, status } = data;
+    this.mijia.log.debug(`${model} ${cmd} voltage->${voltage} status->${status}`);
+    this.setMotionSensor(sid, voltage, temperature)
+  }
+  /**
+   * set up MotionSensor(mijia motion sensor)
+   * @param {*device id} sid 
+   * @param {*device voltage} voltage 
+   * @param {*device status} status 
+   */
+  setMotionSensor(sid, voltage, status) {
+    let uuid = UUIDGen.generate('Mijia-MotionSensor@' + sid);
+    let accessory = this.mijia.accessories[uuid];
+    let service;
+    if (!accessory) {
+      //init a new homekit accessory
+      let name = sid.substring(sid.length - 4);
+      accessory = new PlatformAccessory(name, uuid, Accessory.Categories.SENSOR);
+      accessory.getService(Service.AccessoryInformation)
+        .setCharacteristic(Characteristic.Manufacturer, "Mijia")
+        .setCharacteristic(Characteristic.Model, "Mijia MotionSensor")
+        .setCharacteristic(Characteristic.SerialNumber, sid);
+      accessory.on('identify', function (paired, callback) {
+        callback();
+      });
+      service = new Service.MotionSensor(name);
+      accessory.addService(service, name);
+      accessory.addService(new Service.BatteryService(name), name);
+    } else {
+      service = accessory.getService(Service.MotionSensor);
+    }
+    accessory.reachable = true;
+    if (status != undefined) {
+      service.getCharacteristic(Characteristic.MotionDetected).updateValue('motion' == status);
+    }
+    this.setBatteryService(sid, voltage, accessory);
+    if (!this.mijia.accessories[uuid]) {
+      this.mijia.accessories[uuid] = accessory;
+      this.registerAccessory([accessory]);
+    }
+    return accessory;
   }
 }
+module.exports = Monitor;
